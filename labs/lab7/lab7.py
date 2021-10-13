96a27c8d17387fd35a1ff209bed7d1d39bb1f718
duhakanjo <kanjod@g.cofc.edu>
2021-10-13 15:06:02 -0400

    lab 7

--- labs/lab7/lab7.py
+++ labs/lab7/lab7.py
@@ -3,11 +3,59 @@ Name: <your name goes here – first and last>
 Partner: <your partner's name goes here – first and last>
 <ProgramName>.py
 """
+import math
 
+def cash_conversion():
+    x = eval(input("enter an integer: "))
+    print('${:.2f}'. format(x))
+
+def encode():
+    message = input(" enter the message :")
+    key = eval(input("enter the key :"))
+    acc = ""
+    for i in message:
+        x = ord(i)
+        y = x + key
+        z = chr(y)
+        acc += z
+    print(acc)
+
+def encode_better():
+    s = input("enter the message")
+    k = input("enter the key")
+    acc = ''
+    for i in range(len(s)):
+        c = s[i]
+        key = k[i]
+        key = ord(key) -97
+        y = ord(c) + key
+        z = chr(y)
+        acc += z
+    print(acc)
+
+def sphereArea(radius):
+    return 4 * math.pi * radius **2
+def sphereVolum(radius):
+    return (4/3) * math.pi * radius **3
+
+
+def sumN(n):
+    total = 0
+    for i in range(1, n+1):
+        total += i
+    return total
+def sumCubes(n):
+    total = 0
+    for i in range(1, n+1):
+        total  = i**3
+    return total
 
 def main():
-    # add function calls here
-    pass
-
-
+    print(sphereVolum(5))
+    print(sphereArea(2))
+    print(sumN(6))
+    print(sumCubes(7))
+    encode_better()
+    encode()
+    cash_conversion()
 main()
